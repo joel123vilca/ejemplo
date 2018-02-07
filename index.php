09b7252e10ead474037f4ecf63cf0ab452dc16b8
joel123vilca <joevil23tjq@gmail.com>
2018-02-06 22:38:03 -0500

    metodos estaticos son accesibles sin instanciar una clase

--- index.php
+++ index.php
@@ -7,9 +7,14 @@ use Vehicles\{Car, Truck};
 $car = new Car('max');
 $car->move();
 echo '<br>owner car :'.$car->getOwner();
-
+echo "<br>1<br>";
 $truck = new Truck('alex','Pickup');
 $truck->move();
-echo '<br>owner truck :'.$truck->getOwner();
+//echo '<br>owner truck :'.$truck->getOwner();
+echo "<br>2<br>";
 
+$truck = new Truck('juan','Pickup');
+$truck->move();
+//echo "<br>total truck:".Truck::$count;
+echo "<br> total truck:".Truck::getTotal();
 ?>
--- vehicles/Truck.php
+++ vehicles/Truck.php
@@ -5,14 +5,19 @@ require_once 'VehicleBase.php';
 
 class Truck extends VehicleBase{
     private $type;
+    private static $count = 0;
     public function __construct($ownername, $type){
       $this->type = $type;
       $this->owner =$ownername;
       //parent::__construct($owner);//para usar constructor en las clases hijas se usa la palabra reservada "parent:__construct"
-
+      self:: $count++;
     }
     public function move(){
       echo "<br>TRUCK:moving".$this->type;
+    }
+    public static function getTotal()
+    {
+      return self::$count;
     }
 }
 
